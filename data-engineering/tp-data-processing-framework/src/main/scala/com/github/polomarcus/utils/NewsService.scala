28c593297a0faf22fcf29cff4e8dc8e5aa815d9d
cjassey <clement.jassey@epfedu.Fr>
2024-04-29 22:11:43 +0200

    End TP_data_processing_framework

--- data-engineering/tp-data-processing-framework/src/main/scala/com/github/polomarcus/utils/NewsService.scala
+++ data-engineering/tp-data-processing-framework/src/main/scala/com/github/polomarcus/utils/NewsService.scala
@@ -53,7 +53,7 @@ object NewsService {
    */
   def filterNews(newsDataset: Dataset[News]) : Dataset[News] = {
     newsDataset.filter { news =>
-      ??? //@TODO complete here
+      news.countainsWordGlobalWarming
     }
   }
 
@@ -67,6 +67,6 @@ object NewsService {
    */
   def getNumberOfNews(dataset: Dataset[News]): Long = {
     //@TODO look a the Spark API to know how to count
-    return 1 // code here
+    return dataset.count() // code here
   }
 }
